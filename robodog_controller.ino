dafbd943e2369fdb9ec0c3f09ffd0c1cb826856c
Ian Lansdowne <idlansdowne@gmail.com>
2024-01-01 23:38:43 -0600

    committed unsaved changes

--- robodog_controller.ino
+++ robodog_controller.ino
@@ -13,26 +13,35 @@
 #define BUTTON_UP 10
 #define BUTTON_SEL 14
 
+constexpr float M_PI_8 = M_PI / 8.0f;
+
 // E8:9F:6D:20:30:18
 uint8_t broadcastAddress[] = {0xE8, 0x9F, 0x6D, 0x20, 0x30, 0x18};
 
 const unsigned button_mask = (1 << BUTTON_RIGHT) | (1 << BUTTON_DOWN) |
                              (1 << BUTTON_LEFT) | (1 << BUTTON_UP) |
                              (1 << BUTTON_SEL);
-unsigned btn_states;
+
+union
+{
+  struct
+  {
+    unsigned char right : 7;
+    unsigned char down : 1;
+    unsigned char left : 2;
+    unsigned char up : 1;
+    unsigned char sel : 4;
+  } btn;
+  unsigned masked_btns;
+} btns;
 
 Adafruit_seesaw ss;
 Adafruit_NeoPixel pixel(1, 0, NEO_GRB + NEO_KHZ800);
 
 struct
 {
-  unsigned vertical;
-  unsigned horizontal;
-  bool right;
-  bool down;
-  bool left;
-  bool up;
-  bool sel;
+  float v_x, v_y, yaw, pitch, roll;
+  bool poweroff;
 } message;
 
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
@@ -78,14 +87,19 @@ void setup()
 
 void loop()
 {
-  message.horizontal = ss.analogRead(STICK_H);
-  message.vertical = ss.analogRead(STICK_V);
-  btn_states = ss.digitalReadBulk(button_mask);
-  message.right = btn_states & (1 << BUTTON_RIGHT);
-  message.down = btn_states & (1 << BUTTON_DOWN);
-  message.left = btn_states & (1 << BUTTON_LEFT);
-  message.up = btn_states & (1 << BUTTON_UP);
-  message.sel = btn_states & (1 << BUTTON_SEL);
+  float fwd = ((signed)ss.analogRead(STICK_V) - 512) / 512.0f; // divide by 512
+  float right = ((signed)ss.analogRead(STICK_H) - 512) / 512.0f;
+  message.v_x = abs(fwd) > .02 ? 0.05f * fwd : 0.0f; // Max at .02 m/s
+  message.v_y = abs(right) > .02 ? 0.05f * right : 0.0f;
+  btns.masked_btns = button_mask & ~ss.digitalReadBulk(button_mask);
+
+  message.roll = btns.btn.right  ? -M_PI_8
+                 : btns.btn.left ? M_PI_8
+                                 : 0.0f;
+  message.pitch = btns.btn.up     ? M_PI_8
+                  : btns.btn.down ? -M_PI_8
+                                  : 0.0f;
+  message.poweroff = btns.btn.sel;
 
   esp_now_send(broadcastAddress, (uint8_t *)&message, sizeof(message));
 
